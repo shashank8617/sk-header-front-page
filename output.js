7934a0395718acb26b3ad873df81c691e8eb0856
Shashank <152534793+shashank8617@users.noreply.github.com>
2024-03-12 14:06:22 +0530

    Add files via upload

---
+++ output.js
@@ -0,0 +1,45 @@
+//dom output methods
+document.write("\"hello good morning\"");
+document.write("\'sample'");
+document.write("lalalalala")
+
+document.writeln("\rwelcome to java script");
+document.writeln("java script");
+document.writeln("scripting language");
+document.writeln("scripting language");
+
+
+// developer output
+console.log("i am console")
+
+//pop-output methods
+
+alert("please ride the bike slowly");
+alert("please come to the class slowly");
+
+// confirm
+confirm("are youn 18+");
+confirm("are you human");
+
+//prompt
+
+
+let waterbottle=Number(prompt("enter the height of the water bottle"));
+let waterbottle1=Number(prompt("enter the height of the water bottle"));
+let add=waterbottle+waterbottle1;
+console.log(add);
+
+
+
+var inputValue=prompt("enter the age");
+if(inputValue>18)
+{
+    console.log("come to the heaven")
+}
+else{
+    console.log("grow up")
+}
+
+
+
+
