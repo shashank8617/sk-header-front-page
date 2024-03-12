b4245f048f368dce9452102595f8dacee3012541
Shashank <152534793+shashank8617@users.noreply.github.com>
2024-03-12 14:04:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var a;//declaration
+a=1;//initilization
+var a=1;//decleration and initilization
+console.log(a);//1
+console.log(a);//1
+
+var a;//re-decleration
+a=200;//re-initilization
+var a=200;//re decleratrion and re initilization
+console.log(a);//200
+
+    console.log("hello");
+
+    //let
+    let variable;//decleration
+    myvariable = "your variable"//initilization
+    console.log(myvariable);
+
+    let simpleone=100;
+    simpleone="not simple";
+    console.log(simpleone);
+
+
+    // by default keyword is var before the identifier
+    greeting="hello";
+    console.log(greetingd);
+
+
+    //const
+
+    const pi=3.142; //possible in both declerartion and initilization
+
+    console.log(pi);
+
+    
