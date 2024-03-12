c7e2bb7a07fcfbe67e8bff443d5d65353dfeeb84
Shashank <152534793+shashank8617@users.noreply.github.com>
2024-03-12 14:05:17 +0530

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,35 @@
+function demo()
+{
+    return "hello";
+}
+let d=demo();
+console.log(d);
+
+//the use of generator function
+to make efficient to iterate the value 
+to make asynchronous
+
+//the syntax of generator function
+function test()
+{
+    console.log("this is normal function");
+}
+
+functio* generatorfun()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
